6f96d6411af23dcd2250f3d342781b39701bf988
Nicolas Peña Escarpentier <npescarpentier@gmail.com>
2019-09-05 21:14:14 -0400

    video mapping + switching working

--- src/js/surf.js
+++ src/js/surf.js
@@ -1,10 +1,12 @@
 // global threejs variables
 let container, renderer, camera, scene;
 let controls, loader, timekeep;
-let surface, wire;
-let surf_mat, video_mat;
-let videos, vid_img, vid_imgCntx, vid_tex, vid_mat;
-let bVideosLoaded, bPlaying, bWires;
+let d_light1, d_light2;
+let surface, surf_mat;
+let proj_list;
+let videos, vids_img, vids_cnt, vids_tex, vids_mat;
+let vid_wid, vid_hei;
+let vids_index, bVideosLoaded, bPlaying;
 
 window.addEventListener('load', init);
 
@@ -32,8 +34,9 @@ function init(){
   window.addEventListener('keypress', onKeyPress, true );
 
   timekeep = 0;
-  bWires = true;
   bVideosLoaded = false;
+  proj_list = ["inner_cadence", "peaceful_forest"];
+
   createEnvironment();
   loadVideos();
 
@@ -52,15 +55,22 @@ function onWindowResize(){
   camera.updateProjectionMatrix();
 }
 function onKeyPress( event ) {
-  let key = parseInt( event.keyCode );
+  let key = String.fromCharCode( parseInt( event.keyCode ) );
 
   switch (key) {
-    case 49: // 1
+    case 'p':
       if(bVideosLoaded) {
-        bWires = !bWires;
         bPlaying = !bPlaying;
         updateVideoState();
       }
+      break;
+    case '1':
+      vids_index = 0;
+      updateVideoState();
+      break;
+    case '2':
+      vids_index = 1;
+      updateVideoState();
       break;
     default:
       break;
@@ -73,12 +83,14 @@ function animate() {
   renderer.setAnimationLoop( render );
 }
 function render(){
-  timekeep += 0.03;
+  timekeep += 0.06;
 
   update_surface();
-  if( bPlaying && videos.readyState === videos.HAVE_ENOUGH_DATA ){
-    vid_imgCntx.drawImage( videos, 0, 0 );
-    if( vid_tex ){ vid_tex.needsUpdate = true; }
+  if( bPlaying && videos[vids_index].readyState === videos[vids_index].HAVE_ENOUGH_DATA ){
+    vids_cnt[vids_index].drawImage( videos[vids_index], 0, 0 );
+    if( vids_tex[vids_index] ){
+      vids_tex[vids_index].needsUpdate = true;
+    }
   }
 
   renderer.render( scene, camera );
@@ -90,25 +102,10 @@ function update_surface(){
     let y = Math.floor( i/81 ) +timekeep;
     let z = Math.sin(x/5) * 10 + Math.cos(y/5) * 10;
     surface.geometry.vertices[i].z = z;
-    if(bWires) { wire.geometry.vertices[i].z = z; }
   }
   surface.geometry.computeFaceNormals();
   surface.geometry.normalsNeedUpdate = true;
   surface.geometry.verticesNeedUpdate =true;
-
-  wire.geometry.normalsNeedUpdate = true;
-  wire.geometry.verticesNeedUpdate =true;
-
-  // for( let i=0; i<wire.geometry.vertices.length; i++ ){
-  //   let x = i%81 +timekeep;
-  //   let y = Math.floor( i/81 ) +timekeep;
-  //   // let z = Math.sin( (x+y)/5 ) * 25 + Math.cos( (x+y)/5 ) * 25;
-  //   let z = Math.sin(x/5) * 20 + Math.cos(y/5) * 20;
-  //   wire.geometry.vertices[i].z = z;
-  // }
-  // // wire.geometry.computeFaceNormals();
-  // wire.geometry.normalsNeedUpdate = true;
-  // wire.geometry.verticesNeedUpdate =true;
 }
 
 
@@ -132,24 +129,12 @@ function createEnvironment(){
 	surface.position.set(0, -20, 0);
   scene.add(surface);
 
-  // WIRE SURFACE
-  let wire_geo = new THREE.PlaneGeometry(surf_wid, surf_hei, surf_divX, surf_divY);
-  let wire_mat = new THREE.MeshBasicMaterial({
-    color: 0x888888,
-    wireframe: true,
-    side: THREE.DoubleSide
-  });
-  wire = new THREE.Mesh(wire_geo, wire_mat);
-  wire.rotation.x = Math.PI * 3/2;
-  wire.position.set(0, -20, 0);
-  scene.add(wire);
-
   // LIGHTS!
-	let d_light1 = new THREE.DirectionalLight(0xc200ff, .5);
-  d_light1.position.set( 0, 1, -1 );
+	d_light1 = new THREE.DirectionalLight(0xc200ff, .5);
+  d_light1.position.set( -1, 1, -1 );
 	scene.add(d_light1);
 
-  let d_light2 = new THREE.DirectionalLight(0x15c0ff, .5);
+  d_light2 = new THREE.DirectionalLight(0x15c0ff, .5);
   d_light2.position.set( 1, 1, 0 );
 	scene.add(d_light2);
 }
@@ -159,55 +144,85 @@ function createEnvironment(){
 // === VIDEO
 function loadVideos(){
   bPlaying = false;
+  vids_index = 0;
+  vid_wid = 800;
+  vid_hei = 450;
+
+  let file_ext = "";
+  let file_path = "./src/vids/"
+
+  // CHECK FOR FORMAT
+  v = document.createElement( 'video' );
+  if(v.canPlayType && v.canPlayType('video/webm').replace(/no/, '')) { file_ext = ".webm"; }
+  else { file_ext = ".mp4"; }
+
+
+  // ARRAY INIT
+  videos   = [];
+  vids_img = [];
+  vids_cnt = [];
+  vids_tex = [];
+  vids_mat = [];
   
   // VIDEO FILE LOADING
-  videos = document.createElement( 'video' );
-  videos.src = "./src/vids/innercad_01.mp4";
-  videos.loop = true;
-  videos.addEventListener('loadeddata', () => {
-    videos.pause();
+  for (let i = 0; i < proj_list.length; i++) {
+    let slug = proj_list[i];
+    let file_name = file_path + slug + file_ext;
 
-    // VIDEO IMAGE REFERENCE
-    vid_img = document.createElement( 'canvas' );
-    vid_img.width  = 800;
-    vid_img.height = 450;
+    videos[i] = document.createElement( 'video' );
+    videos[i].src = file_name;
+    videos[i].loop = true;
+    videos[i].addEventListener('loadeddata', () => {
+      videos[i].pause();
+   
+      // VIDEO IMAGE REFERENCE
+      vids_img[i] = document.createElement( 'canvas' );
+      vids_img[i].width  = vid_wid;
+      vids_img[i].height = vid_hei;
+      
+      // VIDEO IMAGE CONTEXT
+      vids_cnt[i] = vids_img[i].getContext( '2d' );
+      vids_cnt[i].fillStyle = "#00000000";
+      vids_cnt[i].fillRect( 0, 0, vid_wid, vid_hei ); 
+   
+      // VIDEO IMAGE TEXTURE
+      vids_tex[i] = new THREE.Texture( vids_img[i] );
+      vids_tex[i].minFilter = THREE.LinearFilter;
+      vids_tex[i].magFilter = THREE.LinearFilter;
+   
+      // VIDEO MATERIAL
+      vids_mat[i] = new THREE.MeshPhongMaterial({
+        map: vids_tex[i],
+        specular: 0xeeeeee,
+        emissive: 0x000000,
+        overdraw: true,
+        side: THREE.DoubleSide
+      });
+   
+      // update boolean
+      bVideosLoaded = true;
 
-    // VIDEO IMAGE CONTEXT
-    vid_imgCntx = vid_img.getContext( '2d' );
-    vid_imgCntx.fillStyle = "#00000000";
-    vid_imgCntx.fillRect( 0, 0, vid_img.width, vid_img.height ); 
-
-    // VIDEO IMAGE TEXTURE
-    vid_tex = new THREE.Texture( vid_img );
-    vid_tex.minFilter = THREE.LinearFilter;
-    vid_tex.magFilter = THREE.LinearFilter;
-
-    // VIDEO MATERIAL
-    vid_mat = new THREE.MeshLambertMaterial({
-      map: vid_tex,
-      overdraw: true,
-      side: THREE.DoubleSide
-    });
-
-    // update boolean
-    bVideosLoaded = true;
-  } , false); 
-  videos.load();
+     //  console.log("> video " + i + " loaded");
+    } , false); 
+    videos[i].load();
+  }
 }
 
 function updateVideoState(){
   // check if video needs to play or not
   if(bPlaying) {
-    videos.play();
+    videos[vids_index].play();
     // assign video material!
-    surface.material = vid_mat;
-    // and hide the wireframe
-    wire.visible = false;
+    surface.material = vids_mat[vids_index];
+    d_light1.intensity = 0.25;
+    d_light2.intensity = 0.25;
   }
   else {
-    videos.pause();
+    videos[vids_index].pause();
     // go back to original material
     surface.material = surf_mat;
-    wire.visible = true;
+    d_light1.intensity = 0.5;
+    d_light2.intensity = 0.5;
   }
+  // console.log("> (" + bPlaying + ") playing video " + vids_index);
 }
